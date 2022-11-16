f10c2576d52a86c3fe875b708082cd622e2b6d01
Omar Muniz <omargmuniz@gmail.com>
2022-11-15 18:21:20 -0700

    created Recipes App
    used xcode 13 to set up cocoapods project

---
+++ Recipes-App/Recipes-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipes-App
+//
+//  Created by Omar Muniz on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
