2b87b9c0951cb6365a74c6e36e4dbb46c5ab9397
Omar Muniz <omargmuniz@gmail.com>
2022-11-22 13:58:00 -0700

    added Parse config to AppDelegate

--- Recipes-App/Recipes-App/AppDelegate.swift
+++ Recipes-App/Recipes-App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "0CCtyAlslcmfWU23KWk6RKvLh9ZY4wd2RJf7mcQJ"
+            $0.clientKey = "FZxEYnwUGxYEsR6mfXPYLJT8qeUWv6df4MOCpjbx"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
         return true
     }
 
